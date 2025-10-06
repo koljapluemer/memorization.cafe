f450976e8e71063dd03b025c1fa24a21c1db1611
Kolja Sam <github@koljapluemer.com>
2025-10-06 10:23:32 +0200

    add PWA capability

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), VitePWA()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'dexie-cloud-addon': fileURLToPath(new URL('./src/stubs/dexie-cloud-addon.ts', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "dexie-cloud-addon": fileURLToPath(
+        new URL("./src/stubs/dexie-cloud-addon.ts", import.meta.url)
+      ),
     },
   },
 });
